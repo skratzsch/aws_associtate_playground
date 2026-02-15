2fd7085374b2b69e7937747152bc0d88c06eff86
skratzsch <skratzsch@me.com>
2026-02-15 17:37:44 +0100

    change gitignore, add backen.tf, add main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# Dummy Resource zum Testen
+resource "aws_s3_bucket" "test" {
+  bucket = "test-bucket-${data.aws_caller_identity.current.account_id}"
+  
+  tags = {
+    Name        = "Test Bucket"
+    Environment = "learning"
+    ManagedBy   = "terraform"
+  }
+}
+
+data "aws_caller_identity" "current" {}
