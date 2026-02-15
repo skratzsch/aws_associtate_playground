bcf3ad66a58971c637c80e03ef3ffb9fc7487226
skratzsch <skratzsch@me.com>
2026-02-15 17:41:58 +0100

    Fix Terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 1.0"
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -16,7 +16,7 @@ provider "aws" {
 # Dummy Resource zum Testen
 resource "aws_s3_bucket" "test" {
   bucket = "test-bucket-${data.aws_caller_identity.current.account_id}"
-  
+
   tags = {
     Name        = "Test Bucket"
     Environment = "learning"
