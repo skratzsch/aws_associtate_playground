4ae6c20e51cc1f5e90a8acdae61ccb57f3c96785
skratzsch <skratzsch@me.com>
2026-02-16 11:41:46 +0100

    Change deployment pipe, remove initial backend.tf and main.tf

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "aws-associate-playground-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
-  }
-}
